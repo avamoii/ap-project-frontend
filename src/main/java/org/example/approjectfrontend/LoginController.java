a608623e6efba53cc088d7c01e297e29192313a2
Yasna <yasna251353@gmail.com>
2025-06-27 21:12:55 +0330

    new changes in logincontroller and signupcontroller

--- src/main/java/org/example/approjectfrontend/LoginController.java
+++ src/main/java/org/example/approjectfrontend/LoginController.java
@@ -4,6 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
+
 
 public class LoginController {
 
@@ -15,12 +22,33 @@ public class LoginController {
 
     @FXML
     void goToSignUp(ActionEvent event) {
+        try {
+            javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(getClass().getResource("/org/example/approjectfrontend/Signup-view.fxml"));
+            javafx.scene.Parent signupRoot = loader.load();
+            javafx.scene.Scene signupScene = new javafx.scene.Scene(signupRoot);
 
+            javafx.stage.Stage currentStage = (javafx.stage.Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(signupScene);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void handleLogin(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
 
+        if (username.isEmpty() || password.isEmpty()) {
+
+            System.out.println("پر کردن هر دو فیلد الزامی است!");
+            return;
+        }
+
+
+        // پاک کردن فیلدها پس از موفقیت
+        usernameField.clear();
+        passwordField.clear();
     }
 
 }
--- src/main/java/org/example/approjectfrontend/SignupController.java
+++ src/main/java/org/example/approjectfrontend/SignupController.java
@@ -2,6 +2,12 @@ package org.example.approjectfrontend;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -16,6 +22,17 @@ public class SignupController {
 
     @FXML
     void gotoLogin(ActionEvent event) {
+      try {
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/approjectfrontend/login-view.fxml"));
+
+        Parent loginRoot = loader.load();
+        Scene loginScene = new Scene(loginRoot);
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(loginScene);}
+      catch (IOException e) {
+          e.printStackTrace();
+      }
 
     }
 
